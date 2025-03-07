e69f27693cb9f3e37ff715d739252452674a6d07
David Alejandro Osorio Sánchez <david.osorio004@mymdc.net>
2025-03-07 17:43:01 -0500

    Agregando tailwind

---
+++ src/app/components/bonsai/bonsai.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bonsai',
+  imports: [],
+  templateUrl: './bonsai.component.html',
+  styleUrl: './bonsai.component.css'
+})
+export class BonsaiComponent {
+
+}
---
+++ src/app/components/clouds/clouds.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clouds',
+  imports: [],
+  templateUrl: './clouds.component.html',
+  styleUrl: './clouds.component.css'
+})
+export class CloudsComponent {
+
+}
