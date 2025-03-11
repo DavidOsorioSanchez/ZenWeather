fbd12ae96e2bb910269612c5ccc7c2b575e88469
David Alejandro Osorio Sánchez <david.osorio004@mymdc.net>
2025-03-11 17:04:56 -0500

    creando los servicios y consultas api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiRequestService } from './service/api-request.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ZenWeather';
+
+  // datos de la API de OpenWeather
+  constructor(private apiRequestService: ApiRequestService) { }
+  
+  ngOnInit() {
+    this.apiRequestService.GetWeatherData("Londres").subscribe(
+      (data) => {
+        console.log('Weather data:', data);
+      },
+      (error) => {
+        console.error('Error fetching weather data:', error);
+      }
+    );
+  }
+
+  
+
 }
---
+++ src/app/service/api-request.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environment';
+
+
+interface UserData {
+  id: string;
+  Location: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiRequestService {
+
+
+
+  // getCurrentLocation(): void {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position: GeolocationPosition) => {
+  //         const latitud = position.coords.latitude;
+  //         const longitud = position.coords.longitude;
+  //         // Utiliza latitud y longitud aquí
+  //         this.GetWeatherData({ lat: latitud, lon: longitud, part: 'minutely,hourly' }).subscribe(
+  //           (data) => {
+  //             console.log('Weather data:', data);
+  //           },
+  //           (error) => {
+  //             console.error('Error fetching weather data:', error);
+  //           }
+  //         );
+  //       },
+  //       (error: GeolocationPositionError) => {
+  //         // Manejar el error de geolocalización
+  //         console.error("Error al obtener la geolocalización:", error);
+  //       }
+  //     );
+  //   } else {
+  //     console.error("Geolocalización no soportada");
+  //   }
+  // }
+
+  private baseUrlUser = environment.URL_USER;
+  // this.http.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitud}&lon=${longitud}&exclude={part}&appid=${environment.APPID_WEATHER}`)
+            
+  constructor(private http: HttpClient) { }
+
+  // estos son los metodos http de la API de OpenWeather
+  GetWeatherData(Location: string): Observable<any> {
+    return this.http.get(`http://api.worldweatheronline.com/premium/v1/weather.ashx?key=${environment.APPID_WEATHER}&q=${Location}&format=json`)
+  }
+
+  // estos son los  metodos http de usuario
+  GetUserData(idUsuario: number): Observable<UserData> {
+    return this.http.get<UserData>(`${this.baseUrlUser}/user/${idUsuario}`);
+  }
+
+  CreateUser(datosUsuario: UserData): Observable<UserData> {
+    return this.http.post<UserData>(`${this.baseUrlUser}/user`, datosUsuario);
+  }
+
+  UpdateUser(idUsuario: number, datosUsuario: UserData): Observable<UserData> {
+    return this.http.put<UserData>(`${this.baseUrlUser}/user/${idUsuario}`, datosUsuario);
+  }
+
+  DeleteUser(idUsuario: number): Observable<any> {
+    return this.http.delete(`${this.baseUrlUser}/user/${idUsuario}`);
+  }
+}
