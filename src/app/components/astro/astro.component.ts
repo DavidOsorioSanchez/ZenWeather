7f7ce3d3c99cff11cc21b647f9692a33aa16234f
David Alejandro Osorio Sánchez <david.osorio004@mymdc.net>
2025-04-23 18:16:57 -0500

    implementando la rotacion del sol

--- src/app/components/astro/astro.component.ts
+++ src/app/components/astro/astro.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-astro',
@@ -8,6 +8,23 @@ import { Component, Input } from '@angular/core';
 })
 export class AstroComponent {
   @Input() cambio: number = 1;
+
+  @ViewChild('astro', { static: true }) astro: ElementRef | undefined;
+
+  ngAfterViewInit() {
+    this.astroMove();
+  }
+
+  astroMove() {
+    const hour = new Date().getHours();
+
+    const position = (hour - 1) * (100 / 24); // 100% / 24 hours = 4.166666666666667% per hour
+    const roundedPosition = Math.round(position);
+    if (this.astro) {
+      this.astro.nativeElement.style.left = roundedPosition + '%';
+      this.astro.nativeElement.style.transform = 'translateX(-50%)';
+    }
+  }
   constructor() { }
 
 }
